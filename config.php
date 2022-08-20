cbffafe73d20af5020092f70e636b295370906f6
Javad <77051275+Javad-313@users.noreply.github.com>
2022-08-20 23:14:02 +0430

    Add files via upload

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+# - get your own app info on https://telegram.org/auth
+const API_ID       = 123456;                       // Integer - Your own API ID
+const API_HASH     = 'abcdefghijklmnopqrstuvwxyz'; // String - Your own api hash
+const REPORT_PEER  = 'your telegram username';     // String or Array or Integer - Where logs must send to?
+const SESSION_NAME = 'account';                    // String - Your session name
+# - Database Configs
+const DB_HOST    = 'localhost';                   // String - Database host
+const DB_USER    = '';                            // String - Database username
+const DB_PASS    = '';                            // String - Database password
+const DB_NAME    = '';                            // String - Database name
+const DB_CHARSET = 'utf8mb4';                     // String - Database charset
+const DB_COLLATE = 'utf8mb4_unicode_ci';          // String - Database collation
+
+# - Session Configs
+const SESSION_URi      = 'tcp://localhost:3306';       // String - The Mysql database URi
+const SESSION_USER     = '';                           // String - The mysql database username
+const SESSION_PASS     = '';                           // String - The mysql database password
+const SESSION_DATABASE = '';                           // String - The mysql database name
+
---
+++ database.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `users`(
+    `userId` INT NOT NULL,
+);
+
+INSERT INTO `users` VALUES(1), (2), (3), (4), (5);
